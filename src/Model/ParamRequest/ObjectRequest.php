3c73ffe0b7aa985cdb3f6e1319905f36146427bc
Leo Viezens <leo.viezens@web.de>
2022-06-22 10:03:22 +0200

    Add object param

---
+++ src/Model/ParamRequest/ObjectRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LeoVie\PhpParamGenerator\Model\ParamRequest;
+
+/** @psalm-immutable */
+class ObjectRequest implements ParamRequest
+{
+    /** @param array<int, ParamRequest> $constructorTypes */
+    private function __construct(private string $bootstrapScriptPath, private string $classFQN, private array $constructorTypes)
+    {}
+
+    /** @param array<int, ParamRequest> $constructorTypes */
+    public static function create(string $bootstrapScriptPath, string $classFQN, array $constructorTypes): self
+    {
+        return new self($bootstrapScriptPath, $classFQN, $constructorTypes);
+    }
+
+    public function getBootstrapScriptPath(): string
+    {
+        return $this->bootstrapScriptPath;
+    }
+
+    public function getClassFQN(): string
+    {
+        return $this->classFQN;
+    }
+
+    /** @return array<int, ParamRequest> */
+    public function getConstructorTypes(): array
+    {
+        return $this->constructorTypes;
+    }
+}
